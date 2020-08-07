662d016321cf7b624cb1fef0ca892381e82fb2ea
Feng <qibeidu@163.com>
2020-08-07 19:19:52 +0800

    添加禁止反向功能

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,7 @@
 const utils = {
 	"timer": null,
 	"count": 0,
+	"oldDeirction": '',
 	/**
 	 * 控制蛇头移动
 	 * @param {object} ele 类对象数组
@@ -8,65 +9,79 @@ const utils = {
 	 * @param {object} food 食物
 	 */
 	move(ele, direction, food) {
-		switch (direction) {
-			case 'left':
-				clearInterval(utils.timer)
-				utils.timer = setInterval(() => {
-					utils.eatFood(ele, food)
-					ele = document.querySelectorAll('.snake')
-					utils.moveSnake(ele)
-					utils.isDead(utils.isStrickWall(ele))
+		if (!utils.isReverse(direction)) {
+			switch (direction) {
+				case 'left':
+					var attr = 'left',
+						rotate = 'rotate(270deg)'
+					utils.howToMove(ele, food, attr, rotate, direction)
+					// clearInterval(utils.timer)
+					// utils.timer = setInterval(() => {
+					// 	utils.eatFood(ele, food)
+					// 	ele = document.querySelectorAll('.snake')
+					// 	utils.moveSnake(ele)
+					// 	utils.isDead(utils.isStrickWall(ele))
 
-					ele[0].style.transform = 'rotate(270deg)'
-					ele[0].style.left = ele[0].offsetLeft - 30 + 'px'
-					utils.isDead(utils.isCrash(ele))
-					// utils.isDead(utils.isStrickWall(ele))
-				}, 80);
-				break;
-			case 'right':
-				clearInterval(utils.timer)
-				utils.timer = setInterval(() => {
-					utils.eatFood(ele, food)
-					ele = document.querySelectorAll('.snake')
-					utils.moveSnake(ele)
-					utils.isDead(utils.isStrickWall(ele))
+					// 	ele[0].style.transform = 'rotate(270deg)'
+					// 	ele[0].style.left = ele[0].offsetLeft - 30 + 'px'
+					// 	utils.isDead(utils.isCrash(ele))
+					// 	// utils.isDead(utils.isStrickWall(ele))
+					// }, 80);
+					break;
+				case 'right':
+					var attr = 'left',
+						rotate = 'rotate(90deg)'
+					utils.howToMove(ele, food, attr, rotate, direction)
+					// clearInterval(utils.timer)
+					// utils.timer = setInterval(() => {
+					// 	utils.eatFood(ele, food)
+					// 	ele = document.querySelectorAll('.snake')
+					// 	utils.moveSnake(ele)
+					// 	utils.isDead(utils.isStrickWall(ele))
 
-					ele[0].style.transform = 'rotate(90deg)'
-					ele[0].style.left = ele[0].offsetLeft + 30 + 'px'
-					utils.isDead(utils.isCrash(ele))
-					// utils.isDead(utils.isStrickWall(ele))
-				}, 80);
-				break;
-			case 'up':
-				clearInterval(utils.timer)
-				utils.timer = setInterval(() => {
-					utils.eatFood(ele, food)
-					ele = document.querySelectorAll('.snake')
-					utils.moveSnake(ele)
-					utils.isDead(utils.isStrickWall(ele))
+					// 	ele[0].style.transform = 'rotate(90deg)'
+					// 	ele[0].style.left = ele[0].offsetLeft + 30 + 'px'
+					// 	utils.isDead(utils.isCrash(ele))
+					// 	// utils.isDead(utils.isStrickWall(ele))
+					// }, 80);
+					break;
+				case 'up':
+					var attr = 'top',
+						rotate = 'rotate(0deg)'
+					utils.howToMove(ele, food, attr, rotate, direction)
+					// clearInterval(utils.timer)
+					// utils.timer = setInterval(() => {
+					// 	utils.eatFood(ele, food)
+					// 	ele = document.querySelectorAll('.snake')
+					// 	utils.moveSnake(ele)
+					// 	utils.isDead(utils.isStrickWall(ele))
 
-					ele[0].style.transform = 'rotate(0deg)'
-					ele[0].style.top = ele[0].offsetTop - 30 + 'px'
-					utils.isDead(utils.isCrash(ele))
+					// 	ele[0].style.transform = 'rotate(0deg)'
+					// 	ele[0].style.top = ele[0].offsetTop - 30 + 'px'
+					// 	utils.isDead(utils.isCrash(ele))
 
-					// utils.isDead(utils.isStrickWall(ele))
-				}, 80);
-				break;
-			case 'down':
-				clearInterval(utils.timer)
-				utils.timer = setInterval(() => {
-					utils.eatFood(ele, food)
-					ele = document.querySelectorAll('.snake')
-					utils.moveSnake(ele)
-					utils.isDead(utils.isStrickWall(ele) )
+					// 	// utils.isDead(utils.isStrickWall(ele))
+					// }, 80);
+					break;
+				case 'down':
+					var attr = 'top',
+						rotate = 'rotate(18 0deg)'
+					utils.howToMove(ele, food, attr, rotate, direction)
+					// clearInterval(utils.timer)
+					// utils.timer = setInterval(() => {
+					// 	utils.eatFood(ele, food)
+					// 	ele = document.querySelectorAll('.snake')
+					// 	utils.moveSnake(ele)
+					// 	utils.isDead(utils.isStrickWall(ele))
 
-					ele[0].style.transform = 'rotate(180deg)'
-					ele[0].style.top = ele[0].offsetTop + 30 + 'px'
-					utils.isDead(utils.isCrash(ele))
+					// 	ele[0].style.transform = 'rotate(180deg)'
+					// 	ele[0].style.top = ele[0].offsetTop + 30 + 'px'
+					// 	utils.isDead(utils.isCrash(ele))
 
-					// utils.isDead(utils.isStrickWall(ele))
-				}, 80);
-				break;
+					// 	// utils.isDead(utils.isStrickWall(ele))
+					// }, 80);
+					break;
+			}
 		}
 	},
 	/**
@@ -124,6 +139,7 @@ const utils = {
 			window.location.reload(true);
 		}
 	},
+	// 撞上自己
 	isCrash(snakes) {
 		for (var i = 1; i < snakes.length; i++) {
 			var top = snakes[0].offsetTop > snakes[i].offsetTop - snakes[i].offsetHeight &&
@@ -134,5 +150,38 @@ const utils = {
 				return true
 			}
 		}
+	},
+	//封装运动函数
+	howToMove(ele, food, attr, rotate, direc) {
+		utils.oldDeirction = direc
+		clearInterval(utils.timer)
+		utils.timer = setInterval(() => {
+			utils.eatFood(ele, food)
+			ele = document.querySelectorAll('.snake')
+			utils.moveSnake(ele)
+			utils.isDead(utils.isStrickWall(ele))
+
+			ele[0].style.transform = rotate
+			switch (direc) {
+				case 'left':
+					ele[0].style[attr] = ele[0].offsetLeft - 30 + 'px'
+					break;
+				case 'right':
+					ele[0].style[attr] = ele[0].offsetLeft + 30 + 'px'
+					break;
+				case 'up':
+					ele[0].style[attr] = ele[0].offsetTop - 30 + 'px'
+					break;
+				case 'down':
+					ele[0].style[attr] = ele[0].offsetTop + 30 + 'px'
+					break;
+			}
+			utils.isDead(utils.isCrash(ele))
+			// utils.isDead(utils.isStrickWall(ele))
+		}, 80);
+	},
+	// 是否反向
+	isReverse(direc) {
+		return utils.oldDeirction === 'up' && direc === 'down' || utils.oldDeirction === 'down' && direc === 'up' || utils.oldDeirction === 'left' && direc === 'right' || utils.oldDeirction === 'right' && direc === 'left'
 	}
 }
